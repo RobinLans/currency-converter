62387830555be5229d23c0c538d928f69458793f
Robin <roblan95@icloud.com>
2021-10-03 21:33:56 +0200

    ad

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { MdCached } from "react-icons/md";
+import { Select, MenuItem, FormControl } from "@material-ui/core";
 
 function Converter() {
   return (
-    <div>
-      <h1>snopp</h1>
+    <div className="container">
+      <FormControl variant="outlined" size="medium">
+        <Select id="select" value="USD">
+          <MenuItem value="USD">USD</MenuItem>
+          <MenuItem value="SEK">SEK</MenuItem>
+        </Select>
+      </FormControl>
+      <input type="text" placeholder="USD" />
+      <button>
+        <MdCached />
+      </button>
+      <input type="text" placeholder="SEK" />
+      <FormControl variant="outlined" size="medium">
+        <Select id="select" value="USD">
+          <MenuItem value="USD">USD</MenuItem>
+          <MenuItem value="SEK">SEK</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
