03ddaf2764cf77ca45f84c8597dd45d9eec31d47
Robin <roblan95@icloud.com>
2021-10-03 20:40:55 +0200

    Introduces Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Converter from "./components/Converter";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <h1>The</h1>
+          <h1 className="pink">Currency</h1>
+          <h1>Converter</h1>
+        </header>
+        <Converter />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Converter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Converter() {
+  return (
+    <div>
+      <h1>snopp</h1>
+    </div>
+  );
+}
+
+export default Converter;
